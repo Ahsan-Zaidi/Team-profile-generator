1ab5af0aeda07f81bfb39ad3a5b89fb42e6d5ca1
Zaidi <zaidiahsan.r@gmail.com>
2022-09-24 16:41:36 -0500

    Prompts

---
+++ index.js
@@ -0,0 +1,96 @@
+//required dependencies
+const fs = require("fs");
+const inquirer = require('inquirer');
+
+//required Classes from libarary
+const manager = require('./lib/Manager');
+const intern = require('./lib/Intern');
+const engineer = require('./lib/Engineer');
+const Employee = require('./lib/Employee');
+
+//prompt to Enter Manager Information w validation
+const teamMembers = [];
+const Role = [];
+const mgrQuestions = [
+    {
+        type: "input",
+        name: "id",
+        message: "Enter Managers ID: ",
+        validate: validID => {
+            if (validID) {
+                let id = parseInt(validID);
+                if (Number.isInteger(id)) {
+                    return true;
+                } else {
+                    console.log("Must be a digit 0-9 only!");
+                    return false;
+                }
+            } else {
+                console.log("ID is required!");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "name",
+        message: "Enter Managers Name: ",
+        validate: validName => {
+            if (validName) {
+                let letters = /^[A-Za-z]+$/;
+                if (validName.match(letters)) {
+                    return true;
+                } else {
+                    console.log("Name should have alphabets only!");
+                    return false;
+                }
+            } else {
+                console.log("Employee Name is Required!");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter Manager Email address: ",
+        validate: validEmail => {
+            if (validEmail) {
+                let email = /\S+@\S+\.\S+/;
+                if (validEmail) {
+                    return true;
+                } else {
+                    console.log("Please Enter a Valid email address!");
+                }
+            } else {
+                console.log('Email is required');
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "Enter Managers office number: ",
+        validate: validOffNumber => {
+            if(validOffNumber) {
+                let num = parseInt(validOffNumber);
+                if (Number.isInteger(num)) {
+                    return true;
+                } else {
+                    console.log("Response must be a number value!")
+                    return false;
+                }
+            } else {
+                console.log("Office Number information is required!");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'newEmployee',
+        message: "Would you like to add another team member?",
+        default: false
+    }
+]
