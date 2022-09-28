9544b9fe331158b18ecbe91b7efafe6574ace36c
Zaidi <zaidiahsan.r@gmail.com>
2022-09-28 17:50:53 -0500

    tests

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,10 +1,23 @@
 const Intern = require('../lib/Intern');
 
-
-test('Can set the school via constructor \'Intern\'', () => {
+//test to set school value into contructor object
+test("Can set the school via constructor", () => {
     const testValue = 'Utsa';
     let e = new Intern(6, "John", "john@gmail.com", testValue);
     expect(e.school).toBe(testValue);
 });
 
-test('')
+//test role function to return Intern object
+test("getRole returns Intern", () => {
+    const testValue = "Intern";
+    let e = new Intern(4, "preach", "preach@gmail.com", "Utsa");
+    expect(e.getRole()).toBe(testValue);
+});
+
+//test to get school from function
+test("can get school via the constructor", () => {
+    const testValue = "BYU";
+    const e = new Intern(9, "preach", "preach@gmail.com", testValue);
+    expect(e.getSchool()).toBe(testValue);
+});
+
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -5,4 +5,18 @@ test('Can set office number with contructor argument', () => {
     const testValue = 205
     let e = new Manager(8, "Ribby", "ribby@gmail.com", testValue);
     expect(e.officeNumber).toBe(testValue);
-})
+});
+
+//test getrole function to get manager
+test("getRole will return \"Manager\"", () => {
+    const testValue = "Manager";
+    const e = new Manager(5, "Ribby", "ribby@gmail.com", 300);
+    expect(e.getRole()).toBe(testValue);
+});
+
+//test to get Officenumber from function
+test("will getOffice return officenumber", () => {
+    const testValue = 450;
+    const e = new Manager(8, "Ribby", "ribby@gmail.com", testValue);
+    expect(e.getOfficeNumber()).toBe(testValue);
+});
